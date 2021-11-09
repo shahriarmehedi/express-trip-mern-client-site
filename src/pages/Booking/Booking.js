60b3bf220a8da9a28403b0c6300dcb640f40e3a5
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2021-11-09 21:26:35 +0600

    new changes

--- src/pages/Booking/Booking.js
+++ src/pages/Booking/Booking.js
@@ -1,10 +1,6 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
-
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import { useParams } from 'react-router';
-import { useForm } from "react-hook-form";
 import useAuth from '../../hooks/useAuth';
 import axios from 'axios';
 
@@ -13,31 +9,53 @@ import axios from 'axios';
 const Booking = () => {
 
     const { user } = useAuth();
-    const { register, handleSubmit, reset } = useForm();
     let { bookingId } = useParams();
 
     const [booking, setBooking] = useState({});
+
+    const { place, _id } = booking;
 
     //  LOADING DATA
     useEffect(() => {
         fetch(`https://dreadful-spirit-92127.herokuapp.com/services/${bookingId}`)
             .then(res => res.json())
             .then(data => setBooking(data))
-    }, []);
+    });
+
+    const initialBookingInfo = { status: "pending", fullname: user?.displayName, useremail: user?.email, userphone: "", useraddress: "" };
+    const [bookingInfo, setBookingInfo] = useState(initialBookingInfo);
 
 
+    const handleOnBlur = e => {
+        const field = e.target.name;
+        const value = e.target.value;
+        const newInfo = { ...bookingInfo };
+        newInfo[field] = value;
+        setBookingInfo(newInfo);
+        console.log(newInfo);
+    }
 
-
-
-    const onSubmit = data => {
+    const handleBookingSubmit = e => {
+        e.preventDefault();
         console.log('confirm booking clicked');
 
-        console.log(data);
-        axios.post('https://dreadful-spirit-92127.herokuapp.com/bookings', data)
+        // COLLECT DATA
+        const placeBooking = {
+            ...bookingInfo,
+            serviceName: place,
+            bookingId: _id
+        }
+
+
+        // SEND TO SERVER
+        console.log(placeBooking);
+
+
+
+        axios.post('https://dreadful-spirit-92127.herokuapp.com/bookings', placeBooking)
             .then(res => {
                 if (res.data.insertedId) {
                     alert('Booked successfully, Now visit My Bookings to view your bookings');
-                    reset();
                 }
             })
 
@@ -53,7 +71,7 @@ const Booking = () => {
                 className="bg-yellow-50 text-white pb-24">
                 <h1 className="pt-10 pb-20 text-center text-3xl font-bold text-gray-700">Confirm Your Booking</h1>
                 <div className="text-gray-800 pb-20 bg-yellow-300 pt-10 rounded-box w-11/12 md:w-5/6 lg:w-2/5 mx-auto" >
-                    <form className=" add-service-form w-5/6 mx-auto " onSubmit={handleSubmit(onSubmit)}>
+                    <form className=" add-service-form w-5/6 mx-auto " onSubmit={handleBookingSubmit}>
                         <div className="w-full py-10 bg-yellow-100 rounded-box mx-auto">
                             <h3 className="text-2xl font-bold">Selected Trip : {booking?.place}, {booking?.country} </h3>
                             <h4> Trip ID: {booking?._id} </h4> <br />
@@ -62,38 +80,45 @@ const Booking = () => {
                             <h4 className="text-2xl"> Person: {booking?.people} </h4>
                         </div> <br />
 
+                        <label className="label">
+                            <span className="label-text text-gray-800">Trip Id:</span>
+                        </label>
+                        <input defaultValue={booking?._id || ''} required name="tripid" onBlur={handleOnBlur} />
 
-                        <input type="hidden" value="Pending" {...register("status")} />
+                        <label className="label">
+                            <span className="label-text text-gray-800">Trip Name:</span>
+                        </label>
+                        <input defaultValue={booking?.place || ''} required name="tripname" onBlur={handleOnBlur} />
 
 
                         <label className="label">
                             <span className="label-text text-gray-800">Your Full Name:</span>
                         </label>
-                        <input value={user?.displayName} required {...register("fullname")} />
+                        <input defaultValue={user?.displayName || ''} required name="fullname" onBlur={handleOnBlur} />
 
 
 
                         <label className="label">
                             <span className="label-text text-gray-800">Email:</span>
                         </label>
-                        <input required type="email" value={user?.email} {...register("useremail")} />
+                        <input required type="email" name="email" defaultValue={user?.email || ''} onBlur={handleOnBlur} />
 
 
 
                         <label className="label">
                             <span className="label-text text-gray-800">Phone:</span>
                         </label>
-                        <input required type="tel" {...register("userphone", { required: true })} />
+                        <input required type="tel" name="userphone" onBlur={handleOnBlur} />
 
 
 
                         <label className="label">
                             <span className="label-text text-gray-800">Address:</span>
                         </label>
-                        <textarea type="textarea" {...register("useraddress", { required: true })} />
+                        <textarea type="textarea" name="useraddress" onBlur={handleOnBlur} />
 
                         <br />
-                        <input className=" bg-gray-700 hover:bg-gray-800 transition duration-300 text-white mt-12 submit-btn" type="submit" value="CONFIRM BOOKING" />
+                        <input className=" bg-gray-700 hover:bg-gray-800 transition duration-300 text-white mt-12 submit-btn" type="submit" defaultValue="CONFIRM BOOKING" />
                     </form>
                 </div>
 
